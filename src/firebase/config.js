aca45440ac0c02fb46f5a45693f167dca08799cd
Mani Movassagh <mani.mgh@gmail.com>
2021-01-26 19:39:24 +0100

    corrected firebase connection and successfuly worked!

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,14 +1,6 @@
 import * as firebase from 'firebase/app'
 
 
-<!-- The core Firebase JS SDK is always required and must be listed first -->
-<script src="https://www.gstatic.com/firebasejs/8.2.4/firebase-app.js"></script>
-
-<!-- TODO: Add SDKs for Firebase products that you want to use
-     https://firebase.google.com/docs/web/setup#available-libraries -->
-<script src="https://www.gstatic.com/firebasejs/8.2.4/firebase-analytics.js"></script>
-
-<script>
   // Your web app's Firebase configuration
   // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 
@@ -24,4 +16,3 @@ import * as firebase from 'firebase/app'
   // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
   firebase.analytics();
-</script>
